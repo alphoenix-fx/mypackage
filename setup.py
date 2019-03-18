e247759271f3f14ee43c2f06f86276b60b35851c
Alfred Maboa <Alfred@explore-datascience.net>
2019-03-18 12:32:35 +0200

    setup changes

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     description='EDSA example python package',
     long_description=open('README.md').read(),
     install_requires=['numpy'],
-    url='https://github.com/alphoenixbiz/mypackage',
+    url='https://github.com/alphoenix_the_wiz/mypackage',
     author='Alfred Maboa',
     author_email='alfimaboa@gmail.com'
 )
