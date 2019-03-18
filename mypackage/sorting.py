79ae44c914421fa7a53e786534b670ef8cf578ab
Alfred Maboa <Alfred@explore-datascience.net>
2019-03-18 16:01:47 +0200

    merge sort update

--- mypackage/sorting.py
+++ mypackage/sorting.py
@@ -44,6 +44,25 @@ def merge_sort(items):
     list
         list of elements in items in ascending order
     '''
+    def merge(A, B):
+        new_list = []
+        while len(A) > 0 and len(B) > 0:
+            if A[0] < B[0]:
+                new_list.append(A[0])
+                A.pop(0)
+            else:
+                new_list.append(B[0])
+                B.pop(0)
+
+        if len(A) == 0:
+            new_list = new_list + B
+        if len(B) == 0:
+            new_list = new_list + A
+
+        return new_list
+
+
+
     len_i = len(items)
     if len_i == 1:
         return items
